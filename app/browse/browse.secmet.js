11188f3cd2d02659b4c3cee4c2072906b20fb459
Kai Blin <kblin@biosustain.dtu.dk>
2016-06-28 16:57:00 +0200

    browse: Get sec met clusters from API server
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- app/browse/browse.secmet.js
+++ app/browse/browse.secmet.js
@@ -22,44 +22,6 @@
           icon: "fa fa-exchange"
         }
       };
-      $scope.tree = [
-        {
-          "id": "nrps",
-          "parent": "#",
-          "text": "NRPS",
-          state: {
-            disabled: true
-          }
-        },
-        {
-          "id": "t1pks",
-          "parent": "#",
-          "text": "Type I PKS",
-          state: {
-            disabled: true
-          }
-        },
-        {
-          id: "lantipeptide",
-          parent: "#",
-          text: "Lanthipeptide",
-          state: {
-            disabled: true,
-          }
-        },
-        {
-          id: "nc_003888_c3",
-          parent: "lantipeptide",
-          text: "NC_003888 Cluster 3",
-          type: "cluster"
-        },
-        {
-          "id": "nc_003888_c10",
-          "parent": "nrps",
-          "text": "NC_003888 Cluster 10",
-          "type": "cluster"
-        }
-      ];
 
       function activate_cb(event, ctx) {
         // Wrap in $apply so angular notices stuff changed.
