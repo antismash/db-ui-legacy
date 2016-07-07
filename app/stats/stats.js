397df5ba19b53be060d0203d1a32916dbe94f471
Kai Blin <kblin@biosustain.dtu.dk>
2016-07-07 14:30:07 +0200

    stats: Add more statistics
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- app/stats/stats.js
+++ app/stats/stats.js
@@ -12,8 +12,15 @@
       var vm = this;
       $http.get('/api/v1.0/stats').then(function(response){
         vm.general_stats = [
-          {'name': 'Secondary Metabolite Clusters', 'value': response.data.num_clusters},
-          {'name': 'Genomes', 'value': response.data.num_genomes}
+          {'name': 'Secondary metabolite clusters', 'value': response.data.num_clusters},
+          {'name': 'Species with most secondary metabolite clusters', 'value': response.data.top_secmet_species},
+          {'name': 'NCBI taxon with most clusters', 'value': response.data.top_secmet_taxon},
+          {'name': 'Clusters in top taxon', 'value': response.data.top_secmet_taxon_count},
+          {'name': 'Genomes', 'value': response.data.num_genomes},
+          {'name': 'Sequences', 'value': response.data.num_sequences},
+          {'name': 'NCBI taxon with most sequences', 'value': response.data.top_seq_taxon},
+          {'name': 'Sequences in top taxon', 'value': response.data.top_seq_taxon_count},
+
         ];
         vm.sec_met_clusters = response.data.clusters;
       })
