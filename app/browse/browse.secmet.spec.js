becb4b4079cb9c51ae02073f03128097a2d4c7cf
Kai Blin <kblin@biosustain.dtu.dk>
2016-07-21 14:37:43 +0200

    browse.secmet: Add unit tests
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

---
+++ app/browse/browse.secmet.spec.js
@@ -0,0 +1,42 @@
+'use strict';
+describe('BrowseSecmetController', function () {
+  beforeEach(function () {
+    module('antismash.db.ui.browse.secmet');
+  });
+
+  var $scope;
+  var $window;
+  var ctrl;
+
+  beforeEach(function () {
+
+    $window = { open: jasmine.createSpy() };
+    module(function ($provide) {
+      $provide.value('$window', $window);
+    });
+
+
+    inject(function ($injector) {
+      var $controller = $injector.get('$controller');
+      $scope = $injector.get('$rootScope').$new();
+
+      ctrl = $controller('BrowseSecmetController', {
+        $scope: $scope, $window: $window
+      });
+    });
+  });
+
+  it('should be defined', function () {
+    expect(ctrl).toBeDefined();
+  });
+
+  it('should start with the active cluster being "none"', function () {
+    expect(ctrl.active).toEqual('none');
+  });
+
+  it('should open the activated cluster in a new window', function () {
+    $scope.activate_cb(null, {node: {id: 'foo1234_c23_nrps'}});
+    expect($window.open).toHaveBeenCalledWith('/output/foo1234/index.html#cluster-23', '_new');
+  })
+
+});
--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,9 @@ module.exports = function(config) {
     files: [
       'vendor/angular/angular.js',
       'vendor/angular-mocks/angular-mocks.js',
+      'vendor/jquery/dist/jquery.js',
+      'vendor/jstree/dist/jstree.js',
+      'vendor/jsTree-directive/jsTree.directive.js',
       'app/**/*.js',
     ],
 
